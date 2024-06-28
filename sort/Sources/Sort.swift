f0470b370f22938c5fee810706e29e1ae04a1874
Charles Kincy <cpk1971@gmail.com>
2024-06-27 20:01:48 -0700

    some sort options

--- sort/Sources/Sort.swift
+++ sort/Sources/Sort.swift
@@ -10,6 +10,16 @@ struct Sort : AsyncParsableCommand {
         transform: URL.init(fileURLWithPath:))
     var inputFile: URL? = nil
 
+    @Flag(
+        name: [.long, .customShort("b")],
+        help: "leading whitespace doesn't affect sorting")
+    var ignoreLeadingWhitespace = false
+
+    @Flag(
+        name: [.long, .short],
+        help: "reverse sort order")
+    var reverseOrder: Bool = false
+
     mutating func run() async throws {
         print("Your file: \(inputFile?.relativePath ?? "<stdin>")")
         try await sort()
@@ -29,6 +39,32 @@ struct Sort : AsyncParsableCommand {
         for try await line in try fileHandle.bytes.lines {
             lines.append(line)
         }
-        lines.sorted().forEach { print($0) }
+
+        var transform = nop
+        
+        // will build these iterably as we have more
+        if(ignoreLeadingWhitespace) {
+            transform = compareIgnoreLeading
+        }
+
+        let direction: StringPredicate = reverseOrder ? (>) : (<)
+        let predicate = composeFrom(transform: transform, predicate: direction)
+        lines.sorted(by: predicate).forEach { print($0) }
     }
+}
+
+internal typealias StringUnaryOperator = (String) -> String
+internal typealias StringPredicate = (String, String) -> Bool
+
+internal func composeFrom(transform: @escaping StringUnaryOperator, predicate: @escaping StringPredicate) -> StringPredicate {
+    { predicate(transform($0), transform($1)) }
+}
+
+internal func nop(_ a: String) -> String {
+    a
+}
+
+@available(macOS 13.0, *)
+internal func compareIgnoreLeading(_ a: String) -> String {
+    a.replacing(#/^ */#, with: "")    
 }
